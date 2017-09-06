2ba732251fb824b60b2b08a207c3cbb138a24698
pjanik <janikpiotrek@gmail.com>
2017-09-06 12:37:50 +0200

    Add height label
    [#150648540]

--- js/components/car-height-line.js
+++ js/components/car-height-line.js
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react'
 import c from '../sim-constants'
 import { Text, Line, Group } from 'react-konva'
 
+const FONT = 'museo-sans, verdana, arial'
+
 export default class CarHeightLine extends PureComponent {
   render () {
     const { sx, sy, carX, carY } = this.props
@@ -12,7 +14,8 @@ export default class CarHeightLine extends PureComponent {
     return (
       <Group>
         <Line points={points} fill={'grey'} stroke={'black'} strokeWidth={1} />
-        <Text x={sx(carX) - 16} y={sy(carY * 0.5) + 15} fontFamily={'Arial'} fontSize={14} text={`${carY.toFixed(2)} m`} rotation={-90} fill='black' />
+        <Text x={sx(carX) - 16} y={sy(carY * 0.5) + 18} fontFamily={FONT} fontStyle='bold' fontSize={14} text={`${carY.toFixed(2)} m`} rotation={-90} fill='black' />
+        <Text x={sx(carX) - 32} y={sy(carY * 0.5) + 18} fontFamily={FONT} fontSize={14} text='Height' rotation={-90} fill='black' />
       </Group>
     )
   }
