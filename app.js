3e983acf250d6c98bd617654901ce9c8bea751a4
pjanik <janikpiotrek@gmail.com>
2017-09-06 18:23:55 +0200

    Update gh-pages

--- app.js
+++ app.js
@@ -20486,7 +20486,7 @@ var GameTarget = function (_PureComponent) {
           height: MARKER_HEIGHT, fill: COLOR }),
         _react2.default.createElement(_reactKonva.Rect, { x: sx(x - width * 0.5), y: sy(0) - BASE_HEIGHT, width: width * pixelMeterRatio, height: BASE_HEIGHT,
           fill: COLOR }),
-        _react2.default.createElement(_reactKonva.Text, { x: sx(x) - 15, y: sy(0) - MARKER_HEIGHT - 15, fontFamily: 'Arial', fontSize: 14, text: x.toFixed(2), fill: 'black' })
+        _react2.default.createElement(_reactKonva.Text, { x: sx(x) - 20, y: sy(0) - MARKER_HEIGHT - 15, fontFamily: 'Arial', fontSize: 14, text: x.toFixed(2) + ' m', fill: 'black' })
       );
     }
   }]);
